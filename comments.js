8eb1f917ffb7881f0af2056c442840f99381e7f2
bernhard-rubow <128823291+bernhard-rubow@users.noreply.github.com>
2024-05-23 09:12:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,24 +1,70 @@
 // create web server
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const comments = require('./comments.json');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// use express middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// read comments.json file
+var comments = require('./comments.json');
 
-// get all comments
-app.get('/api/comments', (req, res) => {
-  res.json(comments);
+// get comments
+app.get('/comments', function (req, res) {
+    res.json(comments);
 });
 
-// add a comment
-app.post('/api/comments', (req, res) => {
-  const comment = {
-    id: comments.length + 1,
-  }
+// post comments
+app.post('/comments', urlencodedParser, function (req, res) {
+    // get new comment
+    var newComment = req.body;
+    // add new comment to comments
+    comments.push(newComment);
+    // write comments to comments.json file
+    fs.writeFile('./comments.json', JSON.stringify(comments), function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    // send new comment
+    res.json(newComment);
 });
+
+app.listen(3000, function () {
+    console.log('Server is running on port 3000');
+});
+
+// Path: comments.html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Comments</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>Comments</h1>
+    <div id="comments"></div>
+    <form id="commentForm">
+        <input type="text" id="name" name="name" placeholder="Name">
+        <input type="text" id="comment" name="comment" placeholder="Comment">
+        <button type="submit">Submit</button>
+    </form>
+    <script>
+        // get comments
+        $.get('http://localhost:3000/comments', function (comments) {
+            comments.forEach(function (comment) {
+                $('#comments').append('<div>' + comment.name + ': ' + comment.comment + '</div>');
+            });
+        });
+
+        // post comment
+        $('#commentForm').submit(function (event) {
+            event.preventDefault();
+            var name = $('#name').val();
+            var comment = $('#comment').val();
+            $.post('http://localhost:3000/comments', { name: name, comment: comment }, function (newComment) {
+                $('#comments').append('<div>' + newComment.name + ': ' + newComment.comment + '</div>');
+            });
+        });
+    </script>
+</body>
+</html>
